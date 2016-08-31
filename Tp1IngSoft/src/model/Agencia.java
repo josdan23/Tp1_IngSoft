969dfa798509927f6dc01dc303cef3d8493b99c7
fernandezPablo <fernandez.pablo44@gmail.com>
2016-08-30 23:03:05 -0300

    Agregada clase Agencia, modificacion de retorno en Reserva, atributo codSalida agregado a Salida

---
+++ Tp1IngSoft/src/model/Agencia.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Fernández Pablo
+ */
+public class Agencia {
+
+    private ArrayList<Paquete> paquetes;
+    private ArrayList<Salida> salidas;
+    private CatalogoDePaquete catalogoPaquetes;
+    private Reserva reserva;
+    private AdaptadorTransporte adaptadorTransporte;
+
+    public Agencia() {
+    }
+
+    public void comenzarNuevaSalida() {
+    }
+
+    public void seleccionarPaquetes(String codPaquete) {
+    }
+
+    public void seleccionarUnidad(int nroUnidad) {
+    }
+
+    public void ingresarFecha(Date fecha) {
+    }
+
+    public void ingresarCupo(int cupo) {
+    }
+
+    public void confirmarSalida() {
+    }
+
+    public void crearReserva() {
+    }
+
+    public void seleccionarSalida(String codSalida) {
+    }
+
+    public void ingresarCantPasajeros(int cantPasajeros) {
+    }
+
+    public void seleccionarButaca(int nroButaca) {
+    }
+
+    public void confirmarReserva() {
+    }
+
+    public void ingresarCliente(String nombre, long dni, String telefono,
+            String correo) {
+    }
+
+}
