e918883c9ccd970fda4712ed6dfbdd3ae51c5e1a
fernandezPablo <fernandez.pablo44@gmail.com>
2016-08-31 03:39:09 -0300

    Vista del caso de uso Crear Salida

--- Tp1IngSoft/src/view/CrearSalida.java
+++ Tp1IngSoft/src/view/CrearSalida.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 /**
@@ -10,12 +5,10 @@ package view;
  * @author josdan
  */
 public class CrearSalida extends javax.swing.JFrame {
-
-    /**
-     * Creates new form CrearSalida
-     */
+    
     public CrearSalida() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -27,30 +20,129 @@ public class CrearSalida extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        panelCrearSalida = new javax.swing.JPanel();
+        lblNuevaSalida = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        cmbPaquetes = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        cmbUnidades = new javax.swing.JComboBox<>();
+        panelFecha = new javax.swing.JPanel();
+        calendario = new com.toedter.calendar.JCalendar();
+        lblCupos = new javax.swing.JLabel();
+        txtCupos = new javax.swing.JTextField();
+        btnCancelar = new javax.swing.JButton();
+        btnConfirmar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(800, 600));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 572, Short.MAX_VALUE)
+        lblNuevaSalida.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        lblNuevaSalida.setText("Crear Nueva Salida");
+
+        jLabel1.setText("PAQUETE");
+
+        jLabel2.setText("UNIDAD");
+
+        panelFecha.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "FECHA"));
+
+        javax.swing.GroupLayout panelFechaLayout = new javax.swing.GroupLayout(panelFecha);
+        panelFecha.setLayout(panelFechaLayout);
+        panelFechaLayout.setHorizontalGroup(
+            panelFechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFechaLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(calendario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(171, 171, 171))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 387, Short.MAX_VALUE)
+        panelFechaLayout.setVerticalGroup(
+            panelFechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelFechaLayout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(calendario, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+
+        lblCupos.setText("CUPOS");
+
+        btnCancelar.setText("CANCELAR");
+
+        btnConfirmar.setText("CONFIRMAR");
+
+        javax.swing.GroupLayout panelCrearSalidaLayout = new javax.swing.GroupLayout(panelCrearSalida);
+        panelCrearSalida.setLayout(panelCrearSalidaLayout);
+        panelCrearSalidaLayout.setHorizontalGroup(
+            panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelCrearSalidaLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblNuevaSalida)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(panelCrearSalidaLayout.createSequentialGroup()
+                .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelCrearSalidaLayout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)))
+                    .addGroup(panelCrearSalidaLayout.createSequentialGroup()
+                        .addGap(38, 38, 38)
+                        .addComponent(lblCupos)))
+                .addGap(43, 43, 43)
+                .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(cmbPaquetes, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(cmbUnidades, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(panelCrearSalidaLayout.createSequentialGroup()
+                        .addComponent(panelFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 408, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 174, Short.MAX_VALUE))
+                    .addComponent(txtCupos))
+                .addGap(54, 54, 54))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelCrearSalidaLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnCancelar)
+                .addGap(119, 119, 119))
+            .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelCrearSalidaLayout.createSequentialGroup()
+                    .addContainerGap(655, Short.MAX_VALUE)
+                    .addComponent(btnConfirmar)
+                    .addGap(10, 10, 10)))
+        );
+        panelCrearSalidaLayout.setVerticalGroup(
+            panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelCrearSalidaLayout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(lblNuevaSalida)
+                .addGap(46, 46, 46)
+                .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(cmbPaquetes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(46, 46, 46)
+                .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(cmbUnidades, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(32, 32, 32)
+                .addComponent(panelFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
+                .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblCupos)
+                    .addComponent(txtCupos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
+                .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(panelCrearSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelCrearSalidaLayout.createSequentialGroup()
+                    .addContainerGap(566, Short.MAX_VALUE)
+                    .addComponent(btnConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(5, 5, 5)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelCrearSalida, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelCrearSalida, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -92,8 +184,19 @@ public class CrearSalida extends javax.swing.JFrame {
         });
     }
 
-   
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnConfirmar;
+    private com.toedter.calendar.JCalendar calendario;
+    private javax.swing.JComboBox<String> cmbPaquetes;
+    private javax.swing.JComboBox<String> cmbUnidades;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lblCupos;
+    private javax.swing.JLabel lblNuevaSalida;
+    private javax.swing.JPanel panelCrearSalida;
+    private javax.swing.JPanel panelFecha;
+    private javax.swing.JTextField txtCupos;
     // End of variables declaration//GEN-END:variables
 }
