7144c0048badfa3b5e66abbdeae7408aec6cd37c
fernandezPablo <fernandez.pablo44@gmail.com>
2016-09-02 00:39:23 -0300

    Se movio esta interface al paquete presenter

---
+++ Tp1IngSoft/src/presenter/ICrearSalida.java
@@ -0,0 +1,27 @@
+package presenter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import model.Paquete;
+import model.Unidad;
+
+/**
+ *
+ * @author Fernández Pablo
+ */
+public interface ICrearSalida {
+
+    void cargarPaquetes(ArrayList<Paquete> paquetes);
+
+    String obtenerPaqueteSeleccionado();
+
+    void cargarUnidades(ArrayList<Unidad> unidades);
+
+    String obtenerUnidadSeleccionado();
+
+    Date obtenerFecha();
+
+    int obtenerCupos();
+
+    void mostrarAlerta(String msj, String tipo);
+}
