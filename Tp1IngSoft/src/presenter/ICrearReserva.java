be8f1e817dca3d308dcc519c10fbf1f18374f5b1
fernandezPablo <fernandez.pablo44@gmail.com>
2016-09-10 00:55:16 -0300

    modificada la firma de algunos metodos

---
+++ Tp1IngSoft/src/presenter/ICrearReserva.java
@@ -0,0 +1,32 @@
+package presenter;
+
+import java.util.ArrayList;
+import model.Butaca;
+import model.Paquete;
+import model.Salida;
+
+/**
+ *
+ * @author Pablo Fernández
+ */
+public interface ICrearReserva {
+
+    void cargarPaquetes(ArrayList<Paquete> paquetes);
+
+    String obtenerPaqueteSeleccionado();
+
+    void cargarSalidas(ArrayList<Salida> salidas);
+
+    String obtenerSalidaSeleccionada();
+
+    void cargarButacas(ArrayList<Butaca> butacas);
+
+    String obtenerNombre();
+
+    long obtenerDni();
+
+    String obtenerTelefono();
+
+    String obtenerCorreo();
+
+}
