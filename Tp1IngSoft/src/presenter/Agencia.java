c93439f10c6e9c1117c2307f5b8170501399062e
fernandezPablo <fernandez.pablo44@gmail.com>
2016-09-09 21:35:14 -0300

    Se eliminaron los metodos pertenecientes a la creacion de reserva

--- Tp1IngSoft/src/presenter/Agencia.java
+++ Tp1IngSoft/src/presenter/Agencia.java
@@ -36,11 +36,6 @@ public class Agencia {
      */
     private Paquete paquete;
     private CatalogoDePaquete catalogoPaquetes;
-    /*este atributo sirve para mantener una referncia a la reserva creada durante
-      el caso de uso Crear Reserva
-     */
-    private Reserva reserva;
-
     /*
         Este atributo sirve para obtener el precio del paquete y agregarlo
         a la reserva.
@@ -116,89 +111,8 @@ public class Agencia {
         }
     }
 
-    public void crearReserva() {
-        this.reserva = new Reserva();
-        this.paquetes = catalogoPaquetes.obtenerPaquetes();
-    }
 
-    public void seleccionarPaquetesReserva(String codPaquete) {
-        for (Paquete p : this.paquetes) {
-            if (p.getCodPaquete().equals(codPaquete)) {
-                this.paquete = p;
-                break;
-            }
-        }
-        if (this.paquete != null) {
-            this.salidas = this.paquete.obtenerSalidasEnVenta();
-        } else {
-            //avisar a la interfaz grafica que no se encontro el paquete
-        }
-    }
 
-    public void seleccionarSalida(String codSalida) {
-        for (Salida s : this.salidas) {
-            if (s.getCodSalida().equals(codSalida)) {
-                this.salida = s;
-                break;
-            }
-        }
-        if (this.salida != null) {
-            this.reserva.agregarSalida(this.salida);
-            this.butacas = FactoriaServicios.
-                    getInstancia().
-                    getAdaptadorTransporte().
-                    obtenerButacas(this.salida.
-                            getUnidad().getNroUnidad());
-        } else {
-            //avisar a la interfaz grafica que no se encontro la salida
-        }
-    }
-
-    public void ingresarCantPasajeros(int cantPasajeros) {
-        ArrayList<Butaca> butacasDisp = new ArrayList<>();
-        for (Butaca b : butacas) {
-            //si el estado de la butaca es true esta ocupado
-            if (!b.isEstado()) {
-                butacasDisp.add(b);
-            }
-        }
-        if (butacasDisp.size() >= cantPasajeros) {
-            this.reserva.setCantPasajeros(cantPasajeros);
-        }
-
-        //ATENCION! FALTA CALCULAR EL TOTAL DE LA RESERVA
-        this.basePaquete = this.paquete.getListaBasePaquetes().get(cantPasajeros - 1);
-
-        this.total = this.basePaquete.getPrecio();
-
-        this.reserva.setTotal(total);
-    }
-
-    public void seleccionarButaca(int nroButaca) {
-        for (Butaca b : this.butacas) {
-            if (b.getNroButaca() == nroButaca && !b.isEstado()) {
-                this.reserva.agregarButaca(b);
-            }
-        }
-    }
-
-    public void ingresarCliente(String nombre, long dni, String telefono,
-            String correo) {
-        this.reserva.ingresarCliente(nombre, dni, telefono, correo);
-    }
-
-    public void confirmarReserva() {
-        int[] nroDeButacas = this.reserva.obtenerButacas();
-        if (FactoriaServicios.
-                getInstancia().
-                getAdaptadorTransporte().
-                reservarButacas(this.salida.getUnidad().
-                        getNroUnidad(), nroDeButacas)) {
-            this.salida.agregarReserva(this.reserva);
-        } else {
-            // avisar a la interfaz grafica que no se pudieron reservas las butacas
-        }
-    }
 
     //COMENTARIO AGREGADO POR DANIEL
 }
