59fc803c63c46cb410c94fd3f409e9ec1437a213
fernandezPablo <fernandez.pablo44@gmail.com>
2016-09-14 00:25:05 -0300

    Agregado metodo almacenarPaquetes

--- Tp1IngSoft/src/model/CatalogoDePaquetesJson.java
+++ Tp1IngSoft/src/model/CatalogoDePaquetesJson.java
@@ -1,10 +1,14 @@
 package model;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonWriter;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import service.IAdaptadorCatalogoDePaquetes;
@@ -23,21 +27,28 @@ public class CatalogoDePaquetesJson implements IAdaptadorCatalogoDePaquetes {
         Gson gson = new Gson();
         try {
             FileReader reader = new FileReader("src/data/Paquetes.json");
-            BufferedReader buffer = new BufferedReader(reader);
-            String jsonString = "";
-            String aux = "";
-            while ((aux = buffer.readLine()) != null) {
-                jsonString += aux;
-            }
             Type listadoPaquetes = new TypeToken<ArrayList<Paquete>>() {
             }.getType();
-            return gson.fromJson(jsonString, listadoPaquetes);
+            return gson.fromJson(reader, listadoPaquetes);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    public void almacenarPaquetes(ArrayList<Paquete> paquetes) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try {
+            FileWriter writer = new FileWriter("src/data/Paquetes.json");
+            String jsonString = gson.toJson(paquetes);
+            writer.write(jsonString);
+            writer.flush();
+            writer.close();
         } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        return null;
     }
 
 }
